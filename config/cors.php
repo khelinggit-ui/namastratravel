6ec2f5cd6644d5feae15a99a7bd5add23f7669c4
asepsr <asepsaypulrohim221@gmail.com>
2026-09-10 14:37:01 +0700

    update cross

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,14 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_values(array_filter(array_map(
-        'trim',
-        explode(',', env(
-            'CORS_ALLOWED_ORIGINS',
-            'https://namastratravel.com,https://www.namastratravel.com,https://namastratravel.my.id,https://admin.namastratravel.my.id,http://localhost:5173,http://127.0.0.1:5173'
-        )
-    )))),
+    'allowed_origins' => [
+        'https://namastratravel.com',
+        'https://www.namastratravel.com',
+        'https://admin.namastratravel.my.id',
+        'https://namastratravel.my.id',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -35,6 +36,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
