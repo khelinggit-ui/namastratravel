da67266e04e671805d6338dd40c70f7579205177
asepsr <asepsaypulrohim221@gmail.com>
2026-09-10 14:22:53 +0700

    update app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -13,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->use([
+            HandleCors::class,
+        ]);
+
         $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR);
 
         $middleware->redirectGuestsTo(fn () => route('admin.login'));
